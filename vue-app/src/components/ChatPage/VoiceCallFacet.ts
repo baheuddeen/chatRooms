8b7dd0616af0975f1a35985d7b35fabcccd14176
baheuddeen <muhammad.baheuddeen@gmail.com>
2024-03-07 12:46:47 +0200

    only turn

--- vue-app/src/components/ChatPage/VoiceCallFacet.ts
+++ vue-app/src/components/ChatPage/VoiceCallFacet.ts
@@ -30,9 +30,6 @@ export default class VoiceCallFacet {
             trickle: false,
             stream: this.stream,
             config: { iceServers: [
-                { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
-                { urls: 'stun:wee-whisper.com:3478' },
                 { urls: 'turn:wee-whisper.com:3478', username: 'turnusesr', credential: '123456'},
             ] },
         });
